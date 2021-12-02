a66b87aef516f5eaeb55dc7866d519572d75a22d
rudietuesdays <rae@throneless.tech>
2021-12-02 11:27:08 -0500

    add next.js, react and material ui

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-const client = require('./client')
+const client = require("./client");
 
 module.exports = {
-  exportPathMap: async function (defaultPathMap) {
+  exportPathMap: async function(defaultPathMap) {
     const paths = await client
       .fetch('*[_type == "post" && defined(slug)].slug.current')
       .then(data =>
         data.reduce(
           (acc, slug) => ({
-            '/': { page: '/' },
+            "/": { page: "/" },
             ...acc,
-            [`/post/${slug}`]: { page: '/post/[slug]', query: { slug } }
+            [`/post/${slug}`]: { page: "/post/[slug]", query: { slug } }
           }),
           defaultPathMap
         )
       )
-      .catch(console.error)
-    return paths
+      .catch(console.error);
+    return paths;
   }
-}
+};
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import groq from "groq";
+import client from "../client";
+
+function Index(props) {
+  const { posts = [] } = props;
+  return (
+    <div>
+      <h1>Welcome to a blog!</h1>
+      {posts.map(
+        ({ _id, title = "", slug = "", _updatedAt = "" }) =>
+          slug && (
+            <li key={_id}>
+              <Link prefetch href="/post/[slug]" as={`/post/${slug.current}`}>
+                <a>{title}</a>
+              </Link>{" "}
+              ({new Date(_updatedAt).toDateString()})
+            </li>
+          )
+      )}
+    </div>
+  );
+}
+
+Index.getInitialProps = async () => ({
+  posts: await client.fetch(groq`
+    *[_type == "post" && publishedAt < now()]|order(publishedAt desc)
+  `)
+});
+
+export default Index;
