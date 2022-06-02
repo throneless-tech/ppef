a02bcf4dec90e7405cb37b73f5883fe294504fc5
rudietuesdays <rae@throneless.tech>
2022-06-02 14:24:13 -0400

    update homepage button links

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ function Index(props) {
             title="Our Approach"
             description="We build power for grassroots movements through our family of progressive initiatives — including Citizen Action, Justice Works, and Alliance for Quality Education — mobilizes New Yorkers to take action and change the balance of power. "
             buttonText="Learn More"
-            buttonLink="#"
+            buttonLink="our-approach"
           />
           <Gallery logos={settings.approachGallery} />
           <Standout image={settings.impactImage} />
@@ -66,7 +66,7 @@ function Index(props) {
             description="Take action now to help create a state filled with thriving communities driven by progressive values. It is a New York where ALL OF US — those upstate and downstate, of every race, gender, and background — can live with dignity, equity, and security… a New York where ALL people can prosper, and thrive."
             bold="This bold vision is possible, but only if we work together."
             buttonText="Support our work"
-            buttonLink="#"
+            buttonLink="support-us"
           />
           <Footer image={settings.footerImage} pages={pages} />
         </Layout>
