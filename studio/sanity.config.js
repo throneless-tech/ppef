3ce25f323de2e6c521f3af28851f2b74f699a3d7
rudietuesdays <rae@throneless.tech>
2023-11-27 22:00:08 -0500

    add ability to switch between workspaces in sanity

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -6,18 +6,37 @@ import { structure } from './deskStructure'
 import { media } from 'sanity-plugin-media'
 
 
-export default defineConfig({
-  name: 'PPEF',
-  title: 'Public Policy and Education Fund of New York',
-  projectId: "n0k318sj",
-  dataset: "production",
-  plugins: [
-    deskTool({ structure }),
-    media(),
-    tags({}),
-    // visionTool(),
-  ],
-  schema: {
-    types: schemaTypes,
+export default defineConfig([
+  {
+    name: 'PPEF',
+    title: 'Public Policy and Education Fund of New York',
+    projectId: "n0k318sj",
+    dataset: "production",
+    basePath: '/production',
+    plugins: [
+      deskTool({ structure }),
+      media(),
+      tags({}),
+      // visionTool(),
+    ],
+    schema: {
+      types: schemaTypes,
+    },
   },
-})
+  {
+    name: 'PPEF-test',
+    title: 'Sandbox - PPEF',
+    projectId: "n0k318sj",
+    dataset: "test",
+    basePath: '/test',
+    plugins: [
+      deskTool({ structure }),
+      media(),
+      tags({}),
+      // visionTool(),
+    ],
+    schema: {
+      types: schemaTypes,
+    },
+  }
+])
